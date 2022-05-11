d7ee01f52c1fbdbdbfa50426915395062f27fb96
lorena-ang <loreang98@hotmail.com>
2022-05-11 00:28:28 -0500

    Agregar txt de tablas y parte de lectura del txt al daemon-server

--- client.c
+++ client.c
@@ -5,18 +5,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 #define PORT 8080
- 
-int main(int argc, char const* argv[])
+
+int main(int argc, char const *argv[])
 {
     int s = 0, valread;
     struct sockaddr_in serv_addr;
     char *message;
-    char buffer[1024] = { 0 };
+    char buffer[1024] = {0};
     char username[20], password[20];
     int opcion;
 
     // CREATE
-    if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
         puts("Socket creation error.");
         return -1;
     }
@@ -25,13 +26,15 @@ int main(int argc, char const* argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
         puts("Invalid address / Address not supported.");
         return -1;
     }
- 
+
     // CONNECT
-    if (connect(s, (struct sockaddr*)&serv_addr,sizeof(serv_addr)) < 0) {
+    if (connect(s, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
         puts("Connection failed.");
         return -1;
     }
@@ -43,63 +46,64 @@ int main(int argc, char const* argv[])
     printf("Contraseña: ");
     scanf("%s", password);
 
-    strcat(message,"auth;");
-    strcat(message,username);
-    strcat(message,";");
-    strcat(message,password);
-    strcat(message,";");
+    strcat(message, "auth;");
+    strcat(message, username);
+    strcat(message, ";");
+    strcat(message, password);
+    strcat(message, ";");
 
     // Send user data and receive response.
     send(s, message, strlen(message), 0);
-    recv(s, buffer , 1024 , 0);
+    recv(s, buffer, 1024, 0);
 
     // If return value is equal to 1 user data is valid, if it is equal to 0 user data is invalid.
-    if (atoi(buffer) == 1) {
+    if (atoi(buffer) == 1)
+    {
         // puts("Connected.");
         printf("\nBase de Datos de Empleados y Departamentos\n");
-	    printf("Opciones:\n");
-	    printf("1. Ver tabla 1 - Empleados.\n");
-	    printf("2. Ver tabla 2 - Departamentos.\n");
-	    printf("3. Hacer query SELECT.\n");
-	    printf("4. Hacer query JOIN.\n");
-	    printf("5. Hacer query INSERT.\n");
-	    printf("0. Salir.\n");
-	    printf("Teclee la opción deseada: ");
-	    
+        printf("Opciones:\n");
+        printf("1. Ver tabla 1 - Empleados.\n");
+        printf("2. Ver tabla 2 - Departamentos.\n");
+        printf("3. Hacer query SELECT.\n");
+        printf("4. Hacer query JOIN.\n");
+        printf("5. Hacer query INSERT.\n");
+        printf("0. Salir.\n");
+        printf("Teclee la opción deseada: ");
+
         scanf("%d", &opcion);
-	    do {
-            switch (opcion) {
-                case 0:
-                    // Enviar 6.
-                    printf("Cerrar.\n");
-                    send(s, "6;", strlen("6;"), 0);
-                    opcion = 6;
-                    break;
-                case 1:
-                    printf("Imprimir tabla 1.\n");
-                    send(s, "1;Datos tabla 1;", strlen("1;Datos tabla 1;"), 0);
-                    // printf("Tabla 1\n");
-                    // impresionTabla1(empleados, contEmp);
-                    break;
-                case 2:
-                    printf("Imprimir tabla 2.\n");
-                    // printf("Tabla 2\n");
-                    // impresionTabla2(departamentos, contDept);
-                    break;
-                case 3:
-                    printf("Select.\n");
-                    // printf("Query SELECT\n");
-                    break;
-                case 4:
-                    printf("Query.\n");
-                    // printf("Query JOIN\n");
-                    break;
-                case 5:
-                    printf("Insert.\n");
-                    // printf("\nQuery INSERT\n");
-                    break;
-                default:
-                    printf("Opción inválida, intente de nuevo.\n");
+        do
+        {
+            switch (opcion)
+            {
+            case 0:
+                // Enviar 6.
+                printf("Cerrar.\n");
+                send(s, "6;", strlen("6;"), 0);
+                opcion = 6;
+                break;
+            case 1:
+                printf("Imprimir tabla 1.\n");
+                send(s, "1;Datos tabla 1;", strlen("1;Datos tabla 1;"), 0);
+                // impresionTabla1(empleados, contEmp);
+                break;
+            case 2:
+                printf("Imprimir tabla 2.\n");
+                // impresionTabla2(departamentos, contDept);
+                break;
+            case 3:
+                printf("Select.\n");
+                // printf("Query SELECT\n");
+                break;
+            case 4:
+                printf("Query.\n");
+                // printf("Query JOIN\n");
+                break;
+            case 5:
+                printf("Insert.\n");
+                // printf("\nQuery INSERT\n");
+                break;
+            default:
+                printf("Opción inválida, intente de nuevo.\n");
             }
             printf("\nBase de Datos de Empleados y Departamentos\n");
             printf("Opciones:\n");
@@ -111,8 +115,10 @@ int main(int argc, char const* argv[])
             printf("0. Salir.\n");
             printf("Teclee la opción deseada: ");
             scanf("%d", &opcion);
-	    } while (opcion != 6);
-    } else {
+        } while (opcion != 6);
+    }
+    else
+    {
         puts("Credenciales inválidas.");
         return 1;
     }
--- daemon-server.c
+++ daemon-server.c
@@ -15,15 +15,39 @@ FILE *ptrLogs;
 
 struct _user
 {
-	char username[20];
-	char password[20];
+    char username[20];
+    char password[20];
 };
 
-int readConfig(struct _user *users) {
+struct _empleado
+{
+    int id;
+    char nombre[50];
+    char apellidos[100];
+    int idDept;
+    char fechaNacim[11];
+    char fechaContrat[11];
+    char ciudad[50];
+    int proyActuales;
+    double salario;
+};
+
+struct _departamento
+{
+    int idDept;
+    char nombre[80];
+    char descripcion[150];
+    int piso;
+    double presupuesto;
+};
+
+int readConfig(struct _user *users)
+{
     char *filename = "/Users/marcelabarajas/Documents/Semester/Programación Avanzada/TC2025-MP2/daemonServerData/config.txt";
     FILE *fp = fopen(filename, "r");
 
-    if (fp == NULL) {
+    if (fp == NULL)
+    {
         fprintf(ptrLogs, "Config.txt error.\n");
         fflush(ptrLogs);
     };
@@ -32,16 +56,20 @@ int readConfig(struct _user *users) {
     char buffer[MAX_LENGTH];
     int i = 0, firstLine = 0;
 
-    while (fgets(buffer, MAX_LENGTH, fp)) {
-        if (firstLine == 0) {
+    while (fgets(buffer, MAX_LENGTH, fp))
+    {
+        if (firstLine == 0)
+        {
             firstLine = 1;
-        } else {
+        }
+        else
+        {
             buffer[strcspn(buffer, "\n")] = 0;
             buffer[strcspn(buffer, "\r")] = 0;
             printf("%s", buffer);
-            char* sp = ";";
-            char* user = strtok(buffer, sp);
-            char* password = strtok(NULL, sp);
+            char *sp = ";";
+            char *user = strtok(buffer, sp);
+            char *password = strtok(NULL, sp);
             strcpy(users[i].username, user);
             strcpy(users[i].password, password);
             i++;
@@ -52,44 +80,225 @@ int readConfig(struct _user *users) {
     return 0;
 };
 
+int readTable1(struct _empleado *empleados)
+{
+    char *filename = "/Users/marcelabarajas/Documents/Semester/Programación Avanzada/TC2025-MP2/daemonServerData/table_1.txt";
+    FILE *fp = fopen(filename, "r");
+
+    if (fp == NULL)
+    {
+        fprintf(ptrLogs, "table_1.txt error.\n");
+        fflush(ptrLogs);
+    };
+
+    char line[300];
+    int contEmp = 0;
+
+    while (!feof(fp))
+    {
+        while (fgets(line, sizeof(line), fp))
+        {
+            int state = 0;
+            int finished = 0;
+
+            // Que no sea la primera línea y que no haya terminado de leer la línea
+            while (contEmp != 0 && state < 5)
+            {
+                char elem[150] = {NULL};
+
+                // Obtener primera palabra antes del ;
+                char *token = strtok(line, ";");
+                strcpy(elem, token);
+
+                while (token != NULL && finished == 0)
+                {
+                    switch (state)
+                    {
+                    case 0:
+                        empleados[contEmp - 1].id = atoi(elem);
+                        break;
+                    case 1:
+                        strcpy(empleados[contEmp - 1].nombre, elem);
+                        break;
+                    case 2:
+                        strcpy(empleados[contEmp - 1].apellidos, elem);
+                        break;
+                    case 3:
+                        empleados[contEmp - 1].idDept = atoi(elem);
+                        break;
+                    case 4:
+                        strcpy(empleados[contEmp - 1].fechaNacim, elem);
+                        break;
+                    case 5:
+                        strcpy(empleados[contEmp - 1].fechaContrat, elem);
+                        break;
+                    case 6:
+                        strcpy(empleados[contEmp - 1].ciudad, elem);
+                        break;
+                    case 7:
+                        empleados[contEmp - 1].proyActuales = atoi(elem);
+                        break;
+                    case 8:
+                        empleados[contEmp - 1].salario = atof(elem);
+                        finished = 1;
+                        break;
+                    default:
+                        printf("done");
+                    }
+                    state++;
+
+                    if (finished != 1)
+                    {
+                        // Obtener palabra antes del ;
+                        token = strtok(NULL, ";");
+                        strcpy(elem, token);
+                    }
+                }
+            }
+
+            // Cambiar de línea de registros
+            contEmp++;
+        }
+
+        fclose(fp);
+    }
+
+    return contEmp;
+}
+
+void printTable1(struct _empleado *empleados, int contEmp)
+{
+}
+
+void printTable1(struct _empleado *empleados, int contEmp)
+{
+    for (int i = 0; i < contEmp; i++)
+    {
+        fprintf(ptrLogs, "%d; %s; %s; %d; %s; %s; %s; %d; %0.f;\n", empleados[i].id, empleados[i].nombre, empleados[i].apellidos, empleados[i].idDept, empleados[i].fechaNacim, empleados[i].fechaContrat, empleados[i].ciudad, empleados[i].proyActuales, empleados[i].salario);
+    }
+}
+
+void printTable2(struct _departamento *departamentos, int contDept)
+{
+    for (int i = 0; i < contDept; i++)
+    {
+        fprintf(ptrLogs, "%d; %s; %s; %d; %.0f;\n", departamentos[i].idDept, departamentos[i].nombre, departamentos[i].descripcion, departamentos[i].piso, departamentos[i].presupuesto);
+    }
+};
+
+int readTable2(struct _departamento *departamentos)
+{
+    char *filename = "/Users/marcelabarajas/Documents/Semester/Programación Avanzada/TC2025-MP2/daemonServerData/table_2.txt";
+    FILE *fp = fopen(filename, "r");
+
+    if (fp == NULL)
+    {
+        fprintf(ptrLogs, "table_2.txt error.\n");
+        fflush(ptrLogs);
+    };
+
+    char line[300];
+    int contDept = 0;
+
+    while (!feof(fp))
+    {
+        while (fgets(line, sizeof(line), fp))
+        {
+            int state = 0;
+            int finished = 0;
+
+            // Que no sea la primera línea y que no haya terminado de leer la línea
+            while (contDept != 0 && state < 5)
+            {
+                char elem[150] = {NULL};
+
+                // Obtener primera palabra antes del ;
+                char *token = strtok(line, ";");
+                strcpy(elem, token);
+
+                while (token != NULL && finished == 0)
+                {
+                    switch (state)
+                    {
+                    case 0:
+                        departamentos[contDept - 1].idDept = atoi(elem);
+                        break;
+                    case 1:
+                        strcpy(departamentos[contDept - 1].nombre, elem);
+                        break;
+                    case 2:
+                        strcpy(departamentos[contDept - 1].descripcion, elem);
+                        break;
+                    case 3:
+                        departamentos[contDept - 1].piso = atoi(elem);
+                        break;
+                    case 4:
+                        departamentos[contDept - 1].presupuesto = atof(elem);
+                        finished = 1;
+                        break;
+                    default:
+                        printf("done");
+                    }
+                    state++;
+
+                    if (finished != 1)
+                    {
+                        // Obtener palabra antes del ;
+                        token = strtok(NULL, ";");
+                        strcpy(elem, token);
+                    }
+                }
+
+                // Cambiar de línea de registros
+                contDept++;
+            }
+        }
+
+        fclose(fp);
+    }
+
+    return contDept;
+}
+
 static void daemonize()
 {
     pid_t pid;
-    
+
     pid = fork();
-    
+
     if (pid < 0)
         exit(EXIT_FAILURE);
-    
+
     if (pid > 0)
         exit(EXIT_SUCCESS);
-    
+
     if (setsid() < 0)
         exit(EXIT_FAILURE);
-    
+
     pid = fork();
-    
+
     if (pid < 0)
         exit(EXIT_FAILURE);
-    
+
     if (pid > 0)
         exit(EXIT_SUCCESS);
-    
+
     umask(027);
-    
+
     chdir("./daemonServerData");
-    
+
     int x;
-    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    for (x = sysconf(_SC_OPEN_MAX); x >= 0; x--)
     {
-        close (x);
+        close(x);
     }
-    
+
     openlog("Daemon-Server", LOG_PID, LOG_DAEMON);
     closelog();
 }
 
-int main() {   
+int main()
+{
     // START DAEMON
     printf("Starting Daemon-Server\n");
     daemonize();
@@ -101,11 +310,12 @@ int main() {
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024] = { 0 };
+    char buffer[1024] = {0};
     // char buffer[3][20];
-   
+
     // CREATE SOCKET
-    if ((s = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((s = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
         fprintf(ptrLogs, "Error creating socket.\n");
         fflush(ptrLogs);
         exit(EXIT_FAILURE);
@@ -115,9 +325,10 @@ int main() {
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
- 
+
     // BIND SOCKET
-    if (bind(s, (struct sockaddr*)&address, sizeof(address)) < 0) {
+    if (bind(s, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         fprintf(ptrLogs, "Bind failed.\n");
         fflush(ptrLogs);
         exit(EXIT_FAILURE);
@@ -134,51 +345,187 @@ int main() {
     struct _user users[10] = {"", ""};
     readConfig(users);
 
-    // ACCEPT CONNECTIONS
-    while ((new_socket = accept(s, (struct sockaddr*)&address, (socklen_t*)&addrlen))) {
-        // GET OPTION
-        
-        while((data = recv(new_socket, buffer , 1024 , 0))) {
-            char* sp = ";";
-            char* option = strtok(buffer, sp);
-            
-            if(atoi(option) == 0) {
-                char* user = strtok(NULL, sp);
-                char* password = strtok(NULL, sp);
+    // STORE DATA FROM TABLES
+    struct _empleado empleados[50] = {NULL, "", "", NULL, "", "", " ", NULL, 0};
+    struct _departamento departamentos[20] = {NULL, "", "", NULL, 0};
+    int contEmp = readTable1(empleados) - 1;
+    int contDept = ReadTable2(departamentos) - 1;
 
-                for (int i = 0; i < 10; i++) {
+    // ACCEPT CONNECTIONS
+    while ((new_socket = accept(s, (struct sockaddr *)&address, (socklen_t *)&addrlen)))
+    {
+        // GET OPTION
+
+        while ((data = recv(new_socket, buffer, 1024, 0)))
+        {
+            char *sp = ";";
+            char *option = strtok(buffer, sp);
+
+            if (atoi(option) == 0)
+            {
+                char *user = strtok(NULL, sp);
+                char *password = strtok(NULL, sp);
+
+                for (int i = 0; i < 10; i++)
+                {
                     int validUsername = strcmp(users[i].username, user);
                     int validPassword = strcmp(users[i].password, password);
-                    
-                    if(validUsername == 0 && validPassword == 0) {
-                        fprintf(ptrLogs, "Connected, user authenticated.\n");            
+
+                    if (validUsername == 0 && validPassword == 0)
+                    {
+                        fprintf(ptrLogs, "Connected, user authenticated.\n");
                         fflush(ptrLogs);
                         send(new_socket, "1", strlen("1"), 0);
                         break;
-                    } else {
+                    }
+                    else
+                    {
                         send(new_socket, "0", strlen("0"), 0);
                     }
                 }
-            } else if (atoi(option) == 1) {
+            }
+            else if (atoi(option) == 1)
+            {
                 // Obtener datos de la tabla 1 - empleados.
-            } else if (atoi(option) == 2) {
+                printTable1(empleados, contEmp);
+            }
+            else if (atoi(option) == 2)
+            {
                 // Obtener datos de la tabla 2 - departamentos.
-            } else if (atoi(option) == 3) {
+                printTable2(departamentos, contDept);
+            }
+            else if (atoi(option) == 3)
+            {
                 // Select.
-            } else if (atoi(option) == 4) {
+            }
+            else if (atoi(option) == 4)
+            {
                 // Join
-            } else if (atoi(option) == 5) {
+            }
+            else if (atoi(option) == 5)
+            {
                 // Insert
-            } else if (atoi(option) == 6) {
+                fprintf(ptrLogs, "\nQuery INSERT\n");
+                fprintf(ptrLogs, "1. INSERT a Tabla 1 - Empleados\n");
+                fprintf(ptrLogs, "2. INSERT a Tabla 2 - Departamentos\n");
+                fprintf(ptrLogs, "0. Regresar al menú\n");
+                fprintf(ptrLogs, "Teclee la opción deseada: ");
+
+                // Read client's table choice
+                int insertOpcion = 0;
+                scanf("%d", &insertOpcion); // No se de donde sacar esta lectura
+                FILE *fp;
+
+                if (insertOpcion == 1)
+                {
+                    FILE *fp = fopen("table_1.txt", "a");
+                    if (fp != NULL)
+                    {
+                        int id, idDept, proyActuales;
+                        double salario;
+                        char temp, nombre[50], apellidos[100], fechaNacim[11], fechaContrat[11], ciudad[50];
+
+                        // No se como obtener las lecturas de estos datos, pero necesito leer un int, string,
+                        // string, int, string, string, string, int, double
+
+                        /*
+                        printf("Id (entero): ");
+                        scanf("%d", &id);
+                        empleados[contEmp].id = id;
+                        printf("Nombre (string): ");
+                        scanf("%c", &temp); // Limpiar buffer
+                        scanf("%[^\n]", nombre);
+                        strcpy(empleados[contEmp].nombre, nombre);
+                        printf("Apellidos (string): ");
+                        scanf("%c", &temp);
+                        scanf("%[^\n]", apellidos);
+                        strcpy(empleados[contEmp].apellidos, apellidos);
+                        printf("Id de Departamento (entero): ");
+                        scanf("%d", &idDept);
+                        empleados[contEmp].idDept = idDept;
+                        printf("Fecha de Nacimiento (Día/Mes/Año): ");
+                        scanf("%c", &temp);
+                        scanf("%s", fechaNacim);
+                        strcpy(empleados[contEmp].fechaNacim, fechaNacim);
+                        printf("Fecha de Contratación (Día/Mes/Año): ");
+                        scanf("%s", fechaContrat);
+                        strcpy(empleados[contEmp].fechaContrat, fechaContrat);
+                        printf("Ciudad (string): ");
+                        scanf("%c", &temp);
+                        scanf("%[^\n]", ciudad);
+                        strcpy(empleados[contEmp].ciudad, ciudad);
+                        printf("Proyectos Actuales (entero): ");
+                        scanf("%d", &proyActuales);
+                        empleados[contEmp].proyActuales = proyActuales;
+                        printf("Salario (double): ");
+                        scanf("%lf", &salario);
+                        empleados[contEmp].salario = salario;
+                        */
+                        fprintf(fp, "\n%d;%s;%s;%d;%s;%s;%s;%d;%.0f;", id, nombre, apellidos, idDept, fechaNacim, fechaContrat, ciudad, proyActuales, salario);
+                        contEmp++;
+
+                        fclose(fp);
+                    }
+                    else
+                    {
+                        fprintf(ptrLogs, "Error opening table_1.txt\n");
+                    }
+                }
+                else if (insertOpcion == 2)
+                {
+                    fp = fopen("table_2.txt", "a");
+                    if (fp != NULL)
+                    {
+                        int idDept, piso;
+                        char temp, nombre[80], descripcion[150];
+                        double presupuesto;
+
+                        // No se como obtener las lecturas de estos datos, pero necesito leer un int, string,
+                        // string, int, double
+
+                        /*
+                        printf("Id (entero): ");
+                        scanf("%d", &idDept);
+                        departamentos[contDept].idDept = idDept;
+                        printf("Nombre (string): ");
+                        scanf("%c", &temp); // Limpiar buffer
+                        scanf("%[^\n]", nombre);
+                        strcpy(departamentos[contDept].nombre, nombre);
+                        printf("Descripción (string): ");
+                        scanf("%c", &temp);
+                        scanf("%[^\n]", descripcion);
+                        strcpy(departamentos[contDept].descripcion, descripcion);
+                        printf("Piso (entero): ");
+                        scanf("%d", &piso);
+                        departamentos[contDept].piso = piso;
+                        printf("Presupuesto (double): ");
+                        scanf("%lf", &presupuesto);
+                        departamentos[contDept].presupuesto = presupuesto;
+                        */
+                        fprintf(fp, "\n%d;%s;%s;%d;%.0f;", idDept, nombre, descripcion, piso, presupuesto);
+                        contDept++;
+
+                        fclose(fp);
+                    }
+                    else
+                    {
+                        fprintf(ptrLogs, "Error in fopen for table_2.txt\n");
+                    }
+                }
+                break;
+            }
+            else if (atoi(option) == 6)
+            {
                 // Cerrar
-                fprintf(ptrLogs, "Close user account.\n");            
+                fprintf(ptrLogs, "Close user account.\n");
                 fflush(ptrLogs);
             }
         }
     }
 
-    if (new_socket < 0) {
-            fprintf(ptrLogs, "Accept failed.\n");
+    if (new_socket < 0)
+    {
+        fprintf(ptrLogs, "Accept failed.\n");
     }
 
     fclose(ptrLogs);
